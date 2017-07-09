1c056b9e4b0027673445f9b5e7917d15706fb59b
luegg <215731713@qq.com>
2017-07-10 07:07:07 +0800

    debug 1

--- db/account_dao.py
+++ db/account_dao.py
@@ -48,7 +48,7 @@ class AccountDAO:
     def query_account(self, account, password=None):
         sql = "SELECT a.*, d.name AS dept FROM %s a " \
               "LEFT JOIN %s d ON a.department_id = d.id " \
-              "WHERE a.account='%s' OR a.login_phone='%s' AND a.status=1" % (self.account_tab, self.dept_tab, account, account)
+              "WHERE (a.account='%s' OR a.login_phone='%s') AND a.status=1" % (self.account_tab, self.dept_tab, account, account)
         if password:
             sql += " AND a.password='%s'" % password
         ret = yield self._executor.async_select(self._get_inst(True), sql)
