fd63ec7ac2f64a81559095bfca8ff21875907745
luegg <215731713@qq.com>
2017-09-29 04:27:09 +0800

    release 15

--- db/mysql_config.py
+++ db/mysql_config.py
@@ -5,8 +5,15 @@ class MySQLConfig:
         host = '127.0.0.1'
         port = 3306
         passwd = '123456'
-        db = 'debug_oa'
-        if 'mode' in kwargs and kwargs['mode'] == 1:
-            host = '203.88.48.251'
-            port = 13306
+        db = 'release_oa'
+        if 'mode' in kwargs:
+            if kwargs['mode'] == 1:
+                host = '203.88.48.251'
+                port = 13306
+            elif kwargs['mode'] == 2:
+                db = 'debug_oa'
+            elif kwargs['mode'] == 3:
+                host = '203.88.48.251'
+                port = 13306
+                db = 'debug_oa'
         self.global_metas = (mysql_inst_mgr.DBMeta(host=host, port=port, user='root', passwd=passwd, db=db),)
