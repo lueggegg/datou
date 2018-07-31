f719cb11f3f3939f196ea5ec71ab0f68c0c3fce7
lugg-lee <luegg701@163.com>
2018-07-31 16:05:20 +0800

    yc

--- handlers/html_handler.py
+++ handlers/html_handler.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from base_handler import BaseHandler
-from tornado import gen, escape
+from base_handler import BaseHandler, HttpClient
+from tornado import gen
 
 class HtmlHandler(BaseHandler):
 
@@ -16,9 +16,24 @@ class HtmlHandler(BaseHandler):
             if not st:
                 return
         else:
-            self.wlog('yc_request from %s' % self.request.remote_ip)
+            self.wlog('yc_request to %s from %s' % (path, self.request.remote_ip))
+            self.push(self.request.remote_ip)
         try:
             arg = self.request.arguments
             self.render(path[1:], account_info=self.account_info, arg=arg)
         except IOError:
             self.send_error(404)
+
+    @gen.coroutine
+    def push(self, ip):
+        client = HttpClient()
+        client.url('http://whois.pconline.com.cn/ip.jsp?ip=%s' % ip)
+        resp = yield client.get()
+        extra = {
+            "type": -1,
+            "job_id": -1,
+            'title': ip,
+            'content': resp.body.strip().decode('gbk'),
+            'sender': 'system'
+            }
+        self.push_server.push_with_alias("", [250], extra)
